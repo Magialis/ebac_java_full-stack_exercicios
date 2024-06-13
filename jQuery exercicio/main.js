b89f9333b48438a191d4e32671b881db8b270e39
Magialis <167231725+Magialis@users.noreply.github.com>
2024-06-13 17:08:37 -0300

    Update main.js

--- jQuery exercicio/main.js
+++ jQuery exercicio/main.js
@@ -12,13 +12,17 @@ $(document).ready(function() {
 
         const nomeDaTarefa = $('#nome-tarefa').val();
         const novaTarefa = $('<li style="display: none"></li>');
-        $(`<div class="sublinhado">
-                <li>
-                    <input type="checkbox" id="checkbox"><span class="text">${nomeDaTarefa}</span>
-                </li>
-            </div>`).appendTo(novaTarefa);
+        $(`<li>
+            <input type="checkbox" id="checkbox"><span class="text">${nomeDaTarefa}</span>
+        </li>`).appendTo(novaTarefa);
         $(novaTarefa).appendTo('ul');
         $(novaTarefa).fadeIn(1000);
         $('#nome-tarefa').val('');
     })
+    
+    //para riscar os itens da lista
+    function toggleClasseRiscado() {
+    $(this).toggleClass('riscado');
+    }
+    $('ul').on('click', 'li', toggleClasseRiscado);
 })
